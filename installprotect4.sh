ead1fcaa0ea5161214c971da8261a34937acd442
hanzaddb <khususaddb@gmail.com>
2025-10-18 21:58:15 +0800

    Update installprotect4.sh

--- installprotect4.sh
+++ installprotect4.sh
@@ -44,7 +44,7 @@ class NodeController extends Controller
         // === 🔒 FITUR TAMBAHAN: Anti akses selain admin ID 1 ===
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, '🚫 Akses ditolak! Hanya admin ID 1 yang dapat membuka menu Nodes. ©Protect By LightSecret V1.3');
+            abort(403, '🚫 Akses ditolak! Hanya admin ID 1 yang dapat membuka menu Nodes.');
         }
         // ======================================================
 
