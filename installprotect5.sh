1c0eb76e25e14b06f3beb5332244622dad6023cf
hanzaddb <khususaddb@gmail.com>
2025-10-18 21:59:36 +0800

    Simplify access denied message for admin menu
    Remove version information from access denied message.

--- installprotect5.sh
+++ installprotect5.sh
@@ -56,7 +56,7 @@ class NestController extends Controller
         // 🔒 Proteksi: hanya user ID 1 (superadmin) yang bisa akses menu Nest
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, '🚫 Akses ditolak! Hanya admin utama (ID 1) yang bisa membuka menu Nests. ©LightSecret Protect t.me/lightsecrett v1.3');
+            abort(403, '🚫 Akses ditolak! Hanya admin utama (ID 1) yang bisa membuka menu Nests.');
         }
 
         return $this->view->make('admin.nests.index', [
