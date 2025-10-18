a1777ee06635a3f8565272a9d99f5e64022209b0
hanzaddb <khususaddb@gmail.com>
2025-10-18 22:04:37 +0800

    Simplify access denial message in installprotect3.sh

--- installprotect3.sh
+++ installprotect3.sh
@@ -56,7 +56,7 @@ class LocationController extends Controller
         // 🔒 Cegah akses selain admin ID 1
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, 'LightSecret Protect - Akses ditolak');
+            abort(403, 'Akses ditolak');
         }
 
         return $this->view->make('admin.locations.index', [
@@ -74,7 +74,7 @@ class LocationController extends Controller
         // 🔒 Cegah akses selain admin ID 1
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, 'LightSecret Protect - Akses ditolak');
+            abort(403, 'Akses ditolak');
         }
 
         return $this->view->make('admin.locations.view', [
@@ -92,7 +92,7 @@ class LocationController extends Controller
         // 🔒 Cegah akses selain admin ID 1
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, 'LightSecret Protect - Akses ditolak');
+            abort(403, 'Akses ditolak');
         }
 
         $location = $this->creationService->handle($request->normalize());
@@ -111,7 +111,7 @@ class LocationController extends Controller
         // 🔒 Cegah akses selain admin ID 1
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, 'LightSecret Protect - Akses ditolak');
+            abort(403, 'Akses ditolak');
         }
 
         if ($request->input('action') === 'delete') {
@@ -135,7 +135,7 @@ class LocationController extends Controller
         // 🔒 Cegah akses selain admin ID 1
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, 'LightSecret Protect - Akses ditolak');
+            abort(403, 'Akses ditolak');
         }
 
         try {
